01b37ebc15bf3e63e6edfcdc51c255cdbcf18aaa
Danil Rudin <ddanilr@yandex.ru>
2025-05-13 17:14:49 +0300

    build: improve docker deploy script

--- Deployer.sh
+++ Deployer.sh
@@ -1,4 +1,15 @@
-docker rm -f dreamerbot || true
+echo "Removing old container if it exists..."
+if [ "$(docker ps -aq -f name=^dreamerbot$)" ]; then
+    docker rm -f dreamerbot
+    echo "Old container removed."
+else
+    echo "No existing container found."
+fi
 
+echo "Building image..."
 docker build --no-cache -t dreamerbot .
+
+echo "Running container..."
 docker run -d --restart=always --name dreamerbot dreamerbot
+
+echo "The container has been successfully run"
